84e6635051887f24e58c44134c4bfc46d005a96e
mahmoud <mahmoud1702810@miuegypt.edu.eg>
2021-06-10 03:08:48 +0200

    first commit

--- app.py
+++ app.py
@@ -9,30 +9,29 @@ import numpy as np
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def crop_check():
-    Data = request.args.to_dict()
-    if Data['type'] == "image":
+    if request.form['type'] == "image":
         np.set_printoptions(suppress=True)
         model = None
         labels = None
-        if Data['crop'] == "tomato":
+        if request.form['crop'] == "tomato":
             model = tensorflow.keras.models.load_model('models/tomato_keras_model2.h5')
             labels = ["Tomato Bacterial_spot", "Tomato Early Blight", "Tomato Healthy", "Tomato Late Blight",
                       "Tomato Leaf Mold", "Tomato Septoria leaf spot", "Tomato Spider mites Two spotted spider mite",
                       "Tomato Target Spot", "Tomato mosaic virus", "Tomato Yellow Leaf Curl Virus"]
-        elif Data['crop'] == "potato":
+        elif request.form['crop'] == "potato":
             model = tensorflow.keras.models.load_model('models/potato_keras_model.h5')
             labels = ["Potato Early Blight", "Potato Healthy", "Potato Late Blight"]
-        elif Data['crop'] == "apple":
+        elif request.form['crop'] == "apple":
             model = tensorflow.keras.models.load_model('models/apple_keras_model.h5')
             labels = ["Apple Scab", "Apple Black rot", "Cedar Apple Rust ", "Apple Healthy"]
-        elif Data['crop'] == "corn":
+        elif request.form['crop'] == "corn":
             model = tensorflow.keras.models.load_model('models/corn_keras_model.h5')
             labels = ["Corn Cercospora (Gray) Leaf Spot", "Corn Common Rust", "Corn Healthy",
                       "Corn Northern Leaf Blight"]
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-        image = Image.open('images/'+Data['image'])
+        image = Image.open(request.files['file'])
         size = (224, 224)
         image = ImageOps.fit(image, size, Image.ANTIALIAS)
         image_array = np.asarray(image)
